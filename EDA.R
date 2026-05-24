060ff4602d60d10a1595b961eaecee792f62a175
Valentina <valentinamelica88@gmail.comgit config --globar credential.helper cachegit config --global user.email valentinamelica88@gmail.com>
2026-05-23 20:16:36 -0400

    made the pie chart, created a table with the stats test and added a column to group the stats test in three main cats

--- EDA.R
+++ EDA.R
@@ -25,12 +25,19 @@ suppressMessages(library(lattice))
 
 # import literature review merged data 
 lit_rev <- read.csv(here("./input/Litreview.csv")) 
+#write.csv(lit_rev_s, "lit_rev_s.csv", row.names = FALSE)
+lit_rev <- lit_rev_s %>%
+  mutate(Stats.group = case_when(Stats.type %in% c("ANCOVA","F-test", "ANOVA ", "T-test", "Test of equal slopes", "Pearson's chi_square" ,"Correlation test") ~ "Slopes comparison",
+                                 Stats.type %in% c("Linear regression", "R-squared ") ~ "Regression analysis",
+                                 Stats.type == "Descriptive stas-CV" ~ "Descriptive",
+                                 TRUE ~ "Other" ))
+
 lit_rev_s <- lit_rev %>%
   rename(refPalme = Reference.number.in.Dr..Palme.list ) %>%
   mutate(refPalme = case_when(refPalme == "not in the list"~ "NA", 
                               TRUE ~ refPalme)) %>%
   filter(Parallelism.reporting != "") %>% #removing ghost rows
-  filter(refPalme != "NA" ) %>% # if we decide to remove the non 2022 papers
+  # filter(refPalme != "NA" ) %>% # if we decide to remove the non 2022 papers
   select(-starts_with("X")) %>% #removing ghost columns
   select(-c(number,Author.list, City, Country, Class, Latin.Species,Species, number....of.fecal.samples.)) #selcting relevant columns
 
@@ -40,11 +47,22 @@ lit_rev_s <- lit_rev %>%
 lit_rev_s %>%
   group_by(Parallelism.reporting) %>%
   summarize(n = n()) %>%
-  mutate(prop = n/ sum(n) *100)
+  mutate(prop = round(n / sum(n) * 100)) %>%
+  ggplot(aes(x = "", y = prop, fill = Parallelism.reporting)) +
+  geom_col(width = 1) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  labs(fill = "Parallelism reporting",
+       title = " ") +
+  geom_text(aes(label = paste0(round(prop, 1), "%")),
+            position = position_stack(vjust = 0.5))
+ggsave(here("./outputs/pie_plot.png"), width = 6, height = 6, dpi = 600)
 
 ggplot(lit_rev_s, aes(x=Parallelism.reporting))+
   geom_bar(stat="count", width=0.7, fill="steelblue")+
   theme_minimal()+ xlab("")
+
+
 
 lit_rev_s %>%
   filter(Parallelism.reporting == "Yes") %>%
@@ -52,26 +70,36 @@ lit_rev_s %>%
   summarize(n = n()) %>%
   mutate(prop = n/ sum(n) *100)
 
-lit_rev_s %>%
-  filter(Parallelism.reporting == "Yes") %>%
-  ggplot(aes(x=Graph))+
-  geom_bar(stat="count", width=0.7, fill="steelblue")+
-  theme_minimal()+ xlab("")
-
+# lit_rev_s %>%
+#   filter(Parallelism.reporting == "Yes") %>%
+#   ggplot(aes(x=Graph))+
+#   geom_bar(stat="count", width=0.7, fill="steelblue")+
+#   theme_minimal()+ xlab("")
 lit_rev_s %>%
   filter(Parallelism.reporting == "Yes") %>%
   group_by(Stats) %>%
   summarize(n = n()) %>%
-  mutate(prop = n/ sum(n) *100)
+  mutate(prop = round(n / sum(n) * 100))
+lit_rev_s %>%
+  filter(Parallelism.reporting == "Yes", Stats != "N") %>%
+  group_by(Stats.type) %>%
+  summarize(n = n()) 
 
+lit_rev_s <- lit_rev_s %>%
+  mutate(Stats.group = case_when(Stats.type %in% c("ANCOVA","F-test", "ANOVA ", "T-test", "Test of equal slopes", "Pearson's chi_square" ,"Correlation test") ~ "Slopes comparison",
+                                 Stats.type %in% c("Linear regression", "R-squared ") ~ "Regression analysis",
+                                 Stats.type == "Descriptive stas-CV" ~ "Descriptive",
+                              TRUE ~ "Other" ))
+
+lit_rev_s %>%
+  filter(Parallelism.reporting == "Yes", Stats == "Y") %>%
+  group_by(Stats.group) %>%
+  summarize(n = n()) 
 
 lit_rev_s %>%
   filter(Parallelism.reporting == "Yes") %>%
   ggplot(aes(x=Stats))+
   geom_bar(stat="count", width=0.7, fill="steelblue")+
   theme_minimal()+ xlab("")
-lit_rev_s %>%
-  filter(Parallelism.reporting == "Yes", Stats != "N") %>%
-  group_by(Stats.type) %>%
-  summarize(n = n()) 
+
 # need to muutate some of the names of the stats type
