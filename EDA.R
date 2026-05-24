da3e30774247885b4062405700830b0f5f645463
Valentina <valentinamelica88@gmail.comgit config --globar credential.helper cachegit config --global user.email valentinamelica88@gmail.com>
2026-05-24 11:54:08 -0400

    Fixed a couple of mistakes in the descriptors and typos in the original file.

--- EDA.R
+++ EDA.R
@@ -19,6 +19,7 @@ suppressMessages(library(patchwork))
 suppressMessages(library(RColorBrewer))
 suppressMessages(library(ggpubr))
 suppressMessages(library(ggplot2))
+
 suppressMessages(library(lattice))
 
 # Import data -----------------------------------------------
@@ -26,21 +27,37 @@ suppressMessages(library(lattice))
 # import literature review merged data 
 lit_rev <- read.csv(here("./input/Litreview.csv")) 
 #write.csv(lit_rev_s, "lit_rev_s.csv", row.names = FALSE)
-lit_rev <- lit_rev_s %>%
-  mutate(Stats.group = case_when(Stats.type %in% c("ANCOVA","F-test", "ANOVA ", "T-test", "Test of equal slopes", "Pearson's chi_square" ,"Correlation test") ~ "Slopes comparison",
-                                 Stats.type %in% c("Linear regression", "R-squared ") ~ "Regression analysis",
-                                 Stats.type == "Descriptive stas-CV" ~ "Descriptive",
-                                 TRUE ~ "Other" ))
+colnames(lit_rev)
+# clean up original file
+lit_rev%>%
+  filter(Parallelism.reporting == "Yes", Stats != "N") %>%
+  group_by(Stats.type) %>%
+  summarize(n = n())
 
-lit_rev_s <- lit_rev %>%
-  rename(refPalme = Reference.number.in.Dr..Palme.list ) %>%
+lit_rev <- lit_rev %>%
+  filter(number != 127) %>% # remove one row that need to be excluded
+  rename(refPalme = Reference.number.in.Dr..Palme.list ) %>% # rename some columns that have long names
+  rename(DilutionLin = Do.they.test.Dilution.linearity. ) %>%
   mutate(refPalme = case_when(refPalme == "not in the list"~ "NA", 
                               TRUE ~ refPalme)) %>%
-  filter(Parallelism.reporting != "") %>% #removing ghost rows
+  filter(Parallelism.reporting != "") %>% # removing ghost rows
   # filter(refPalme != "NA" ) %>% # if we decide to remove the non 2022 papers
-  select(-starts_with("X")) %>% #removing ghost columns
-  select(-c(number,Author.list, City, Country, Class, Latin.Species,Species, number....of.fecal.samples.)) #selcting relevant columns
+  select(-starts_with("X")) %>% # removing ghost columns
+  mutate(Stats.group = case_when(Stats.type %in% c("ANCOVA","F-test", "ANOVA ", "T-test", "Test of equal slopes", "Pearson's chi_square" ,"Correlation test") ~ "Slopes comparison",
+                                 Stats.type %in% c("Linear regression", "R-squared ") ~ "Regression analysis",
+                                 Stats.type == "Descriptive stats-CV" ~ "Descriptive",
+                                 TRUE ~ "Other" )) %>% # add statistical group column
+  select(-c(number,Author.list, City, Country, Class, Latin.Species,Species, number....of.fecal.samples.)) # selecting relevant columns
 
+lit_rev%>%
+  group_by(Stats.group) %>%
+  summarize(n = n())
+
+write.csv(lit_rev, here("./input/Litreview_rev.csv"), row.names = FALSE) # save as a new csv
+
+lit_rev_s<- read.csv(here("./input/Litreview_rev.csv")) 
+
+  
 # colnames(lit_rev_s) # to check
 
 # summary of parallelism reporting
@@ -58,50 +75,44 @@ lit_rev_s %>%
             position = position_stack(vjust = 0.5))
 ggsave(here("./outputs/pie_plot.png"), width = 6, height = 6, dpi = 600)
 
-ggplot(lit_rev_s, aes(x=Parallelism.reporting))+
-  geom_bar(stat="count", width=0.7, fill="steelblue")+
-  theme_minimal()+ xlab("")
+###### PROPORTION OF STUDIES THAT APPLIED STATS TEST 
 
-
-
-lit_rev_s %>%
-  filter(Parallelism.reporting == "Yes") %>%
-  group_by(Graph) %>%
-  summarize(n = n()) %>%
-  mutate(prop = n/ sum(n) *100)
-
-# lit_rev_s %>%
-#   filter(Parallelism.reporting == "Yes") %>%
-#   ggplot(aes(x=Graph))+
-#   geom_bar(stat="count", width=0.7, fill="steelblue")+
-#   theme_minimal()+ xlab("")
 lit_rev_s %>%
   filter(Parallelism.reporting == "Yes") %>%
   group_by(Stats) %>%
   summarize(n = n()) %>%
   mutate(prop = round(n / sum(n) * 100))
+# List of stats test used- saved in table format as csv
 Stats_test <- lit_rev_s %>%
   filter(Parallelism.reporting == "Yes", Stats != "N") %>%
   group_by(Stats.type) %>%
   summarize(n = n()) 
-write.csv(Stats_test , "Stats_test.csv", row.names = FALSE)
 
+Stats_test <- Stats_test %>%
+  rename(Statistical_test =Stats.type )
+write.csv(Stats_test , here("./outputs/Stats_test.csv"), row.names = FALSE)
+# adding dilution linearity as a stats group ( NOTE: 3 studies have applied dilution linearity, 
+#two of them did it in addition to a statistical test for equal slopes, therefore they already have a stats group assigned)
 
-lit_rev_s <- lit_rev_s %>%
-  mutate(Stats.group = case_when(Stats.type %in% c("ANCOVA","F-test", "ANOVA ", "T-test", "Test of equal slopes", "Pearson's chi_square" ,"Correlation test") ~ "Slopes comparison",
-                                 Stats.type %in% c("Linear regression", "R-squared ") ~ "Regression analysis",
-                                 Stats.type == "Descriptive stas-CV" ~ "Descriptive",
-                              TRUE ~ "Other" ))
+lit_rev_s <-lit_rev_s %>%
+mutate(Stats.group = case_when(Stats.group == "Other" & 
+                    DilutionLin == "Y" ~ paste0(Stats.group, "_Diliution Linearity"),
+                    TRUE ~ Stats.group))
 
 lit_rev_s %>%
-  filter(Parallelism.reporting == "Yes", Stats == "Y") %>%
+  filter(Stats.group !="Other") %>%
   group_by(Stats.group) %>%
-  summarize(n = n()) 
+  summarize(n = n())  %>%
+  mutate(prop = round(n / sum(n) * 100)) %>%
+  ggplot(aes(x= Stats.group, y= prop))+
+  geom_bar(stat= "identity", width=0.7, fill="steelblue")+
+  theme_minimal()+ ylab("proportion of studies (%)") + xlab("")
+ggsave(here("./outputs/stats_group.png"), width = 6, height = 6, dpi = 600)
 
-lit_rev_s %>%
-  filter(Parallelism.reporting == "Yes") %>%
-  ggplot(aes(x=Stats))+
-  geom_bar(stat="count", width=0.7, fill="steelblue")+
-  theme_minimal()+ xlab("")
 
 # need to muutate some of the names of the stats type
+lit_rev_s %>%
+  filter(Parallelism.reporting == "Yes") %>%
+  group_by(Graph) %>%
+  summarize(n = n()) %>%
+  mutate(prop = n/ sum(n) *100)
